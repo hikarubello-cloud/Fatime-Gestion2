61e99bce0ab1f402b8e6040685f68b308c092a46
hikarubello-cloud <hikarubello@gmail.com>
2026-03-05 04:46:10 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,21 @@
+const CACHE_NAME = "fatime-cache-v1";
+
 self.addEventListener("install", event => {
-  console.log("App installée");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
